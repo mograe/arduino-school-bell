80294acc511e3b4e2f3dcd5e6b35fd3a93112abc
antontim4enko <anton.tim4enko.2014@gmail.com>
2019-09-28 09:57:26 +0300

    refactor code

--- bell.ino
+++ bell.ino
@@ -13,24 +13,23 @@ RTCDateTime dt;
 //============== CONSTANTS ==============
 
 bool btnState;
+bool toggleState;
 
 int btnPin = 12;
 int FIRE_ALARM = 2;
 int SCHOOL_BELL = 1;
-
 int TOGGLE_PIN = 7;
-int toggleState;
-bool isLong = true;
+
 int BELLS_LENGTH = 8;
 
-struct bell
+struct Bell
 {
   int hour;
   int minute;
   char title[30];
 };
 
-struct bell longBells[] = {
+struct Bell longBells[] = {
     8, 30, "1 Lesson",
     9, 50, "Short break",
     10, 0, "2 Lesson",
@@ -40,7 +39,7 @@ struct bell longBells[] = {
     13, 20, "4 Lesson",
     14, 40, "HOME"};
 
-struct bell shortBells[] = {
+struct Bell shortBells[] = {
     8, 30, "1 Lesson",
     9, 30, "Short break",
     9, 40, "2 Lesson",
@@ -50,9 +49,82 @@ struct bell shortBells[] = {
     12, 10, "4 Lesson",
     13, 10, "HOME"};
 
+
+class SchoolAlarm
+{
+private
+  bool isLong = true;
+
+  void displayTitle(Bell bell)
+  {
+    lcd.setCursor(0, 1);
+    lcd.print(bell.title);
+  }
+
+  bool ifCurrentLesson(Bell bell)
+  {
+    return dt.hour == bell.hour && dt.minute == bell.minute && dt.second < 10;
+  }
+
+public
+
+  void alarm(int alarmCode)
+  {
+    delay(100);
+    mp3_play(alarmCode);
+    delay(100);
+  }
+
+  void displayTime()
+  {
+    lcd.setCursor(0, 0);
+    dt = clock.getDateTime();
+    lcd.print(dt.hour);
+    lcd.setCursor(2, 0);
+    lcd.print(":");
+    lcd.setCursor(3, 0);
+    lcd.print(dt.minute);
+    lcd.setCursor(5, 0);
+    lcd.print(":");
+    lcd.setCursor(6, 0);
+    lcd.print(dt.second);
+    if (dt.second == 59)
+    {
+      lcd.setCursor(7, 0);
+      lcd.print(' ');
+    }
+    lcd.setCursor(10, 0);
+    lcd.print(isLong ? "Long " : "Short");
+  }
+
+  void toggleSchedule()
+  {
+    isLong = !isLong;
+    delay(300);
+  }
+
+  void schedule(Bell bells[])
+  {
+    for (int n = 0; n < BELLS_LENGTH; n++)
+    {
+      if (ifCurrentLesson(bells[n]))
+      {
+        alarm(SCHOOL_BELL);
+        displayTitle(bell);
+      };
+    };
+  }
+
+  bool isLongModeActive()
+  {
+    return isLong;
+  }
+}
+
 //============== MAIN SETUP ==============
 
-void setup()
+void
+setup()
 {
   pinMode(btnPin, INPUT_PULLUP);
   pinMode(TOGGLE_PIN, INPUT_PULLUP);
@@ -70,6 +142,8 @@ void setup()
   delay(100);
 }
 
+SchoolAlarm schoolAlarm;
+
 //============== MAIN LOOP ==============
 
 void loop()
@@ -79,75 +153,13 @@ void loop()
 
   if (toggleState == LOW)
   {
-    isLong = !isLong;
-    delay(300);
+    schoolAlarm.toggleSchedule();
   }
 
   if (btnState == LOW)
   {
-    alarm(FIRE_ALARM);
+    schoolAlarm.alarm(FIRE_ALARM);
   }
 
-  timeToLCD();
-}
-
-//============== CUSTOM VOIDS ==============
-
-void alarm(int alarmCode)
-{
-  delay(100);
-  mp3_play(alarmCode);
-  delay(100);
-}
-
-void timeToLCD()
-{
-  lcd.setCursor(0, 0);
-  dt = clock.getDateTime();
-  lcd.print(dt.hour);
-  lcd.setCursor(2, 0);
-  lcd.print(":");
-  lcd.setCursor(3, 0);
-  lcd.print(dt.minute);
-  lcd.setCursor(5, 0);
-  lcd.print(":");
-  lcd.setCursor(6, 0);
-  lcd.print(dt.second);
-  if (dt.second == 59)
-  {
-    lcd.setCursor(7, 0);
-    lcd.print(' ');
-  }
-  lcd.setCursor(10, 0);
-  if (isLong == true)
-  {
-    lcd.print("Long ");
-  }
-  else
-  {
-    lcd.print("Short");
-  }
-
-  if (isLong == true)
-  {
-    schedule(longBells);
-  }
-  else
-  {
-    schedule(shortBells);
-  }
-}
-
-void schedule(bell bells[])
-{
-
-  for (int n = 0; n < BELLS_LENGTH; n++)
-  {
-    if (dt.hour == bells[n].hour && dt.minute == bells[n].minute && dt.second < 10)
-    {
-      alarm(SCHOOL_BELL);
-      lcd.setCursor(0, 1);
-      lcd.print(bells[n].title);
-    };
-  };
+  schoolAlarm.schedule(schoolAlarm.isLongModeActive() ? longBells : shortBells);
 }
